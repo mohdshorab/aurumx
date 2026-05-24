f16ed63d8661aeff0944f9e31018f6f380d08837
Mohd Shorab <mohdshorab474@gmail.com>
2026-05-24 18:24:13 +0530

    setup: folder structure and env configurations

---
+++ src/features/dashboard/Dashboard.tsx
@@ -0,0 +1,11 @@
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const Dashboard: React.FC = () => {
+  return (
+    <SafeAreaView>
+      <Text>AURUM X</Text>
+    </SafeAreaView>
+  );
+};
+export default Dashboard;
